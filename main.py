7695c6c2c96a4e2b0ac80e4dd9c4beb619aee47d
Kyousque <cleysongama05@gmail.com>
2021-04-21 22:58:16 -0300

    Bot do Disc

---
+++ main.py
@@ -0,0 +1,23 @@
+import discord
+import random
+
+TOKEN = "ODM0NTgwMDIyODExMDMzNjIw.YIC9Nw.o8YazsHSZ-uqpgFfS-IEPu_L4LA"
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("Bot ON")
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith(".oi"):
+        await message.channel.send("Fala meu patrão!")
+
+    if message.content.startswith(".r"):
+        await message.channel.send(random.randint(1,10))
+
+client.run(TOKEN)
