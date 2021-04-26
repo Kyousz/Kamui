82395492e34e69dcb6f1ac8c524007f48e25650e
Kyousque <cleysongama05@gmail.com>
2021-04-26 18:50:51 -0300

    versão 1.2 - .ajuda, .r e .d

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import discord
 from discord.ext import commands
 import random
-import re
 
 TOKEN = "ODM0NTgwMDIyODExMDMzNjIw.YIC9Nw.o8YazsHSZ-uqpgFfS-IEPu_L4LA"
 
@@ -9,8 +8,8 @@ client = commands.Bot(command_prefix = ".", case_insensitive = True)
 
 @client.event
 async def on_ready():
-  print("{0.user.name} está online!".format(client))
-  await client.change_presence(activity=discord.Game(name="RPG | .ajuda"))
+    print("{0.user.name} está online!".format(client))
+    await client.change_presence(activity=discord.Game(name="RPG | .ajuda"))
 
 @client.command()
 async def ajuda(ctx):
@@ -18,42 +17,38 @@ async def ajuda(ctx):
         title="LISTA DE COMANDOS",
         description="Todos os comandos de Kamui!",
         color=discord.Color.purple())
-    embed.set_author(name="Kyous", url="https://twitter.com/_Kyousz",
-                     icon_url="https://pbs.twimg.com/profile_images/1336294229270736898/6bpP84te_400x400.jpg")
-    embed.add_field(name="**.r [dado] [mod]**",
+    embed.set_author(name="Kyous ~ O Criador", icon_url="https://pbs.twimg.com/profile_images/1336294229270736898/6bpP84te_400x400.jpg")
+    embed.add_field(name="**.r [dados] [mod]**",
                     value="Rolar um ou mais dados com mesma quantidade de lados somando ou diminuindo modificadores.", inline=False)
     embed.add_field(name="**.d [danoMIN] [danoMAX]**",
                     value="Rolar o dano de uma arma ou magia acrescentando apenas o dano mínimo e o máximo.", inline=False)
-    embed.set_footer(text="Versão: 1.0")
+    embed.set_footer(text="Versão: 1.2")
     await ctx.send(embed=embed)
 
 @client.command()
-async def t(ctx, test:str, mod:int=0):
-    x = (test.split("+"))
-    for n in range(0,len(x)):
-        dado, valor = (int(texto) for texto in x[n].split("d"))
-        rolls = [random.randint(1, valor) for i in range(dado)]
-        result = " + ".join([str(r) for r in rolls])
-        simb = "+" if mod > 0 else ""
-    await ctx.send(f"🎲 ***Rolagem de {ctx.author.mention}***"
-                   f"\n**Resultado**: {result} **({simb}{mod})**"
-                   f"\n**Total**: {sum(rolls)+mod}")
+async def metas(ctx):
+    await ctx.send("- Fazer rolagens como: 1d20+1d6+2"
+                   "\n- Deixar números máximos e mínimos em destaque.")
 
 @client.command()
-async def r(ctx, num:str, mod:int=0):
-  dado, valor = (int(texto) for texto in num.split("d"))
-  rolls = [random.randint(1, valor) for i in range(dado)]
-  result = " + ".join([str(r) for r in rolls])
-  simb = "+" if mod > 0 else ""
-  await ctx.send(f"🎲 ***Rolagem de {ctx.author.mention}***"
-                 f"\n**Resultado**: {result} **({simb}{mod})**"
-                 f"\n**Total**: {sum(rolls)+mod}")
+async def r(ctx, num:str="", mod:int=0):
+    lista = []
+    x = (num.split("+"))
+    for n in range(0,len(x)):
+        dado, valor = (int(a) for a in x[n].split("d"))
+        rolagens = [random.randint(1, valor) for i in range(dado)]
+        lista.extend(rolagens)
+        result = " + ".join([str(r) for r in lista])
+    simb = "+" if mod > 0 else ""
+    await ctx.send(f"🎲 ***Rolagem de {ctx.author.mention}***"
+                   f"\n**Resultado**: {num} ({result}) {simb} {mod}"
+                   f"\n**Total**: {sum(lista)+mod}")
 
 @client.command()
 async def d(ctx, min, max):
-  resultado = random.randint(int(min),int(max))
-  await ctx.send(f"🎲 ***Rolagem de Dano: {ctx.author.mention}***"
-                 f"\n**Min/Max**: {min} ~ {max} "
-                 f"\n**Resultado**: {resultado}")
+    resultado = random.randint(int(min),int(max))
+    await ctx.send(f"🎲 ***Rolagem de Dano: {ctx.author.mention}***"
+                   f"\n**Min/Max**: {min} ~ {max} "
+                   f"\n**Resultado**: {resultado}")
 
 client.run(TOKEN)
